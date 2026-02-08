11b4e9c5833b6e999c9885bc2e03e7616ddc4033
Visali <elangovanvisali@gmail.com>
2026-02-08 20:20:08 +0530

    patterns completed

---
+++ pat11.java
@@ -0,0 +1,15 @@
+public class pat11 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            int val = (i % 2 == 1) ? 1 : 0;   
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(val + " ");
+                val = 1 - val;   
+            }
+            System.out.println();
+        }
+    }
+}
